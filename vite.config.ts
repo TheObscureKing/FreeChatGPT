2dae75899553ce74877d36778cf33c3030081926
TheObscureKing <105104667+TheObscureKing@users.noreply.github.com>
2023-03-19 12:36:41 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://theobscureking.github.io/FreeChatGPT/',
   plugins: [react()],
   resolve: {
     alias: {
